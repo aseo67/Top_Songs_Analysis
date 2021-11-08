e41b5cd35198e716ac9bf4e2a89148d8aab587bb
aseo67 <85456812+aseo67@users.noreply.github.com>
2021-11-07 18:00:00 -0800

    Update data loading

--- Create_Table_Script.sql
+++ Create_Table_Script.sql
@@ -1,33 +1,33 @@
 -- CREATE TABLES FOR LOADING DATA
 CREATE TABLE top_songs (
-	Song_Name VARCHAR, 
+	Index VARCHAR, 
 	Song_ID VARCHAR,
+	Song_Name VARCHAR, 
 	Artist VARCHAR, 
 	Number_of_Times_Charted INT,
 	Highest_Charting_Position INT, 
+	Week_of_Highest_Charting_start DATE,
+	Week_of_Highest_Charting_end DATE,
+	number_weeks_charted INT,
 	Streams INT, 
 	Artist_Followers INT, 
 	Genre VARCHAR,
 	Release_Date DATE,
 	Popularity NUMERIC, 
+	Acousticness NUMERIC,
 	Danceability NUMERIC, 
 	Energy NUMERIC,
+	Liveness NUMERIC,
 	Loudness NUMERIC,
 	Speechiness NUMERIC,
-	Acousticness NUMERIC,
-	Liveness NUMERIC,
-	TEMPO NUMERIC,
-	Duration INT,
 	Valence NUMERIC,
+	Tempo NUMERIC,
+	Duration_ms INT,
 	Chord VARCHAR,
-	Week_of_Highest_Charting_start DATE,
-	Week_of_Highest_Charting_end DATE,
-	number_weeks_charted int,
 	PRIMARY KEY (Song_ID)
 );
 
 -- SELECT ALL DATA FOR EACH TABLE
-
 SELECT * FROM top_songs;
 
 -- IF NEED TO DELETE TO RECREATE TABLE
