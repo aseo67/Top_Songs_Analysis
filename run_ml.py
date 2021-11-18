5ff9b02952212956b692daf4055d213a675c1afe
aseo67 <85456812+aseo67@users.noreply.github.com>
2021-11-17 16:26:52 -0800

    Update model

--- run_ml.py
+++ run_ml.py
@@ -1,6 +1,8 @@
 # Import dependencies
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+from collections import Counter
+from imblearn.over_sampling import SMOTE
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
@@ -33,21 +35,24 @@ def predictions(
     # Split preprocessed data into training and testing datasets
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
+    # Data resampled with SMOTE
+    X_resampled, y_resampled = SMOTE(random_state=1, sampling_strategy='auto').fit_resample(X_train, y_train)
+
     # Create StandardScaler instances
     scaler = StandardScaler()
     # Fit the StandardScaler
-    X_scaler = scaler.fit(X_train)
+    X_scaler = scaler.fit(X_resampled)
     # Scale data
-    X_train_scaled = X_scaler.transform(X_train)
+    X_train_scaled = X_scaler.transform(X_resampled)
     X_test_scaled = X_scaler.transform(X_test)
 
     # Create a random forest classifier
     rf_model = RandomForestClassifier(n_estimators=100, random_state=1)
     # Fitting the model
-    rf_model = rf_model.fit(X_train_scaled, y_train)
+    rf_model = rf_model.fit(X_train_scaled, y_resampled)
 
     # Training score & Testing score
-    print(f'Training Data Score: {rf_model.score(X_train_scaled, y_train)}')
+    print(f'Training Data Score: {rf_model.score(X_train_scaled, y_resampled)}')
     print(f'Testing Data Score: {rf_model.score(X_test_scaled, y_test)}')
 
     # Return result
