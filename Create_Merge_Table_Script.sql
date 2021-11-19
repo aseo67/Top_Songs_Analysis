16641672c70758a28c1119e8b2d0e4e5726961a2
aseo67 <85456812+aseo67@users.noreply.github.com>
2021-11-18 21:03:55 -0800

    Update data loading/table manipulation

--- Create_Merge_Table_Script.sql
+++ Create_Merge_Table_Script.sql
@@ -1,4 +1,4 @@
--- CREATE TABLES FOR LOADING DATA
+-- TABLE FOR RAW SPOTIFY TOP 200 CHART (U.S) DATA SCRAPE
 CREATE TABLE raw_scrape (
 	index VARCHAR, 
 	song_id VARCHAR,
@@ -10,37 +10,7 @@ CREATE TABLE raw_scrape (
 	streams VARCHAR
 )
 
-CREATE TABLE songs (
-	song_id VARCHAR,
-	song VARCHAR, 
-	artist VARCHAR, 
-	streams INT,
-	position INT, 
-	danceability NUMERIC, 
-	energy NUMERIC,
-	key VARCHAR,
-	loudness NUMERIC,
-	mode VARCHAR, 
-	speechiness NUMERIC,
-	acousticness NUMERIC,
-	instrumentalness NUMERIC,
-	liveness NUMERIC,
-	valence NUMERIC,
-	tempo NUMERIC,
-	duration_ms NUMERIC,
-	time_signature VARCHAR,
-	PRIMARY KEY ("song_id")
-);
-
-CREATE TABLE clean_scrape (
-	song_id VARCHAR,
-	song VARCHAR,
-	artist VARCHAR, 
-	date DATE, 
-	position INT, 
-	streams INT
-);
-
+-- TABLE FOR SPOTIFY API PULL OF EACH SONG'S FEATURES
 CREATE TABLE features (
 	song_id VARCHAR, 
 	danceability NUMERIC,
@@ -59,18 +29,31 @@ CREATE TABLE features (
 	PRIMARY KEY ("song_id")
 );
 
+-- TABLE FOR CLEANED (via jupyter notebook file) SCRAPE DATA 
+CREATE TABLE clean_scrape (
+	song_id VARCHAR,
+	song VARCHAR,
+	artist VARCHAR, 
+	date DATE, 
+	position INT, 
+	streams INT
+);
+
+-- TABLE OF TOTAL STREAMS DATA FOR EACH SONG
 CREATE TABLE total_streams (
 	song_id VARCHAR, 
 	streams INT, 
 	PRIMARY KEY ("song_id")
 );
 
+-- TABLE OF HIGHEST POSITION RANKING FOR EACH SONG
 CREATE TABLE highest_position (
 	song_id VARCHAR, 
 	position INT, 
 	PRIMARY KEY ("song_id")
 );
 
+-- TABLE LISTING EACH TRACK & ARTIST & SONG ID
 CREATE TABLE track_artist (
 	song_id VARCHAR, 
 	song VARCHAR, 
@@ -78,6 +61,35 @@ CREATE TABLE track_artist (
 	PRIMARY KEY ("song_id")
 );
 
+-- SCRIPT FOR JOINING total_streams, highest_position, track_artist TABLES AND features TABLE (via SQL)
+SELECT DISTINCT ON (ta.song_id) ta.song_id, 
+	ta.song, 
+	ta.artist, 
+	ts.streams, 
+	hp.position, 
+	f.danceability, 
+	f.energy, 
+	f.key, 
+	f.loudness, 
+	f.mode, 
+	f.speechiness, 
+	f.acousticness, 
+	f.instrumentalness, 
+	f.liveness, 
+	f.valence, 
+	f.tempo, 
+	f.duration_ms, 
+	f.time_signature
+INTO songs
+FROM track_artist AS ta
+JOIN total_streams AS ts
+ON (ta.song_id = ts.song_id)
+JOIN highest_position AS hp
+ON (ta.song_id = hp.song_id)
+JOIN features AS f
+ON (ta.song_id = f.song_id);
+
+-- CREATE TABLE FOR FINAL MACHINE LEARNING MODEL DATAFRAME
 CREATE TABLE song_ml (
 	index VARCHAR, 
 	danceability NUMERIC, 
@@ -93,22 +105,22 @@ CREATE TABLE song_ml (
 	PRIMARY KEY ("index")
 );
 
--- SELECT ALL DATA FOR EACH TABLE
+-- SELECT ALL DATA SCRIPT FOR EACH TABLE
 SELECT * FROM raw_scrape;
-SELECT * FROM songs;
 SELECT * FROM clean_scrape;
 SELECT * FROM features;
 SELECT * FROM total_streams;
 SELECT * FROM highest_position;
 SELECT * FROM track_artist;
+SELECT * FROM songs;
 SELECT * FROM song_ml;
 
--- IF NEED TO DELETE TO RECREATE TABLE
+-- SCRIPT IN CASE NEED TO DELETE ANY TABLES
 DROP TABLE raw_scrape;
-DROP TABLE songs;
 DROP TABLE clean_scrape;
 DROP TABLE features;
 DROP TABLE total_streams;
 DROP TABLE highest_position;
 DROP TABLE track_artist;
+DROP TABLE songs;
 DROP TABLE song_ml;
